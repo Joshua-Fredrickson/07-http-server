86c072d7bc4a862e6b5e07f44577ceed950eb335
Joshua Fredrickson <uafredrickson@gmail.com>
2018-04-24 15:20:50 -0700

    scaffolding

---
+++ src/lib/client.js
@@ -0,0 +1,12 @@
+'use strict';
+
+
+// module.exports = class ClientC {
+//   constructor(name)
+//   this.name = name;
+//   userID = `user~ID~${name}`;
+//   this.socket = null;
+// }
+
+// Josh - This constructor passes a name, create an ID of user~ID~<name> and connects a null sockect
+// until it is re-assigned
